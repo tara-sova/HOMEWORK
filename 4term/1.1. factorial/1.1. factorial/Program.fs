58e18c1437b4cf43f72f9ae18f52957ca94a2bf4
tara-sova <tarpol19@gmail.com>
2015-02-25 19:56:20 +0300

    T_T

---
+++ 4term/1.1. factorial/1.1. factorial/Program.fs
@@ -0,0 +1,7 @@
+﻿let rec fact n =
+    if n < 0 then None
+    elif n = 1 then Some 1
+    else 
+        match fact (n - 1) with
+        | Some x -> Some (n * x)
+        | None -> None
---
+++ 4term/1.2. finonacci/1.2. finonacci/Program.fs
@@ -0,0 +1,7 @@
+﻿let rec fibonacci n =
+    if n < 0 then None
+    elif n = 0 || n = 1 || n = 2 || n = 3 then Some 1
+    else 
+        match fibonacci n with
+        | Some x -> Some(x - 1) + Some(x - 2)
+        | None -> None
