8c79809ebbc81b4ee326136439f5c7a4ad59d20e
tara-sova <tarpol19@gmail.com>
2015-02-25 22:31:05 +0300

    - or +? +!

--- 4term/1.2. finonacci/1.2. finonacci/Program.fs
+++ 4term/1.2. finonacci/1.2. finonacci/Program.fs
@@ -1,7 +1,10 @@
 ﻿let rec fibonacci n =
     if n < 0 then None
-    elif n = 0 || n = 1 || n = 2 || n = 3 then Some 1
-    else 
-        match fibonacci n with
-        | Some x -> Some(x - 1) + Some(x - 2)
+    elif n = 0 || n = 1 || n = 2 then Some 1
+    else
+        match fibonacci (n - 1) with
         | None -> None
+        | Some x -> Some(x + (match fibonacci(n - 2) with 
+                              | None -> None 
+                              | Some x -> Some x).Value
+                              )
