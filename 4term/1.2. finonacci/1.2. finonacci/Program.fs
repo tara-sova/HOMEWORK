5ef9cedf1378f9c5d59ac860616a2682a7b63c50
tara-sova <tarpol19@gmail.com>
2015-02-27 19:18:42 +0300

    it is a little bit different now

--- 4term/1.2. finonacci/1.2. finonacci/Program.fs
+++ 4term/1.2. finonacci/1.2. finonacci/Program.fs
@@ -1,10 +1,11 @@
 ﻿let rec fibonacci n =
     if n < 0 then None
     elif n = 0 || n = 1 || n = 2 then Some 1
-    else
+    else 
         match fibonacci (n - 1) with
-        | None -> None
-        | Some x -> Some(x + (match fibonacci(n - 2) with 
-                              | None -> None 
-                              | Some x -> Some x).Value
-                              )
+            | None -> None
+            | Some x -> 
+                        let previous =  fibonacci(n - 2)
+                        match previous with
+                        | None -> None
+                        | Some y -> Some (x + y)
