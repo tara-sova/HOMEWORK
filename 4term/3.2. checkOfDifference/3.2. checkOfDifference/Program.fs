49682e5c002ffbd8f41dffbd2ac2e846ce5f101b
tara-sova <tarpol19@gmail.com>
2015-03-02 13:16:59 +0300

    maybe it works

---
+++ 4term/3.2. checkOfDifference/3.2. checkOfDifference/Program.fs
@@ -0,0 +1,16 @@
+﻿let rec compair (list) head repeat =
+    if ((List.head list) = head) then true
+       else
+           if ((List.length list) = 1) then repeat
+           else
+               compair list.Tail head repeat
+
+let rec smth list repeat = 
+    if ((List.length list) = 1) then repeat
+    else 
+        let repeat = compair (list.Tail) (list.Head) repeat
+        smth list.Tail repeat
+
+let action list =
+    let repeat = false
+    smth list repeat
