4a4b941d77b10d7a7398f9131640aa17d8e95815
tara-sova <tarpol19@gmail.com>
2015-03-28 00:43:38 +0300

    that's not really good

---
+++ 4term/3.6. seqInfinite/3.6. seqInfinite/Program.fs
@@ -0,0 +1,9 @@
+﻿let rec div n lessN = 
+    if (n = 1 || n = 0) then 0
+    elif (lessN = 1) then n
+    elif (n % lessN = 0) then 0
+    else div n (lessN - 1)
+
+let seqInfinite = Seq.initInfinite (fun index ->
+     div index (index - 1) ) |> Seq.distinct |> Seq.skip 1 |> Seq.take 10
+printfn "%A" seqInfinite
