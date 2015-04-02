211d99054e8f4ed7513a0ae6e5dc8c781de542cc
tara-sova <tarpol19@gmail.com>
2015-04-03 00:45:19 +0300

    fact of today's test is a greatest motivation

---
+++ 4term/4.2. pointFree/4.2. pointFree/Program.fs
@@ -0,0 +1,13 @@
+﻿let func x l = List.map (fun y -> y * x) l
+
+let func1 : int -> int list -> int list =
+    fun x -> List.map (fun y -> y * x)
+
+let func2 : int -> int list -> int list =
+    fun x -> List.map (fun y -> ((*) x) y)
+
+let func3 : int -> int list -> int list =
+    fun x -> List.map ((*) x)
+
+let func4 : int -> int list -> int list =
+    (*) >> List.map
