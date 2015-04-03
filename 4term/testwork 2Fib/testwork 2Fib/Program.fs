8e4a27cb76ec8ffce56ea549684efbed9d1e8096
tara-sova <tarpol19@gmail.com>
2015-04-03 12:52:55 +0300

    test

---
+++ 4term/testwork 2Fib/testwork 2Fib/Program.fs
@@ -0,0 +1,20 @@
+﻿let isEven (n : int) = 
+    if (n % 2 = 0) then n
+    else 0
+
+let rec fib n =
+    if n <= 1
+    then 1
+    else fib(n - 1) + fib(n - 2)    
+
+let rec sum (list : int list) (count : int) = 
+    if (fib(count) <= 1000000) then
+       let listWithAdd = (isEven(fib (count))) :: list
+       sum listWithAdd (count + 1)
+    else
+        list
+
+let act = 
+    let list = sum [] 0
+    let result = List.sum list
+    result
---
+++ 4term/testwork tree/testwork tree/Program.fs
@@ -0,0 +1,23 @@
+﻿type Tree<'a> =
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    | Tip of 'a
+
+let rec go (tree : Tree<'a>) (list : 'a list) (f: 'a -> bool)= 
+    match tree with
+    | Tree (value, left, right) -> let res = go (Tip value) list f @ go left list f @ go right list f
+                                   res
+    | Tip(value) -> 
+                    if (f value = true) then (value :: list)
+                    else list
+
+let isEven n = 
+    if (n % 2 = 0) then true
+    else false
+
+let result (tree : Tree<int>) (f: 'a -> bool)= 
+    let res = go tree [] f
+    res
+
+let myTree = Tree(0, Tree(1, Tip(2), Tip(3)), Tip(4))
+result myTree isEven
+
