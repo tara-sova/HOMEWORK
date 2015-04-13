6874795157270d82432220dccfa4753891b42327
tara-sova <tarpol19@gmail.com>
2015-04-13 16:46:26 +0300

    I did it very quickly so there are two options: 1) f# <3 2) "It doesn't work. Rewrite it."

---
+++ 4term/4.5. mapForTree/4.5. mapForTree/Program.fs
@@ -0,0 +1,12 @@
+﻿type Tree<'a> =
+    | Tree of 'a * Tree<'a> * Tree<'a>
+    | Tip of 'a
+
+let rec mapTree (tree  : 'a Tree) (f : 'a -> 'b) = 
+    match tree with
+    | Tree (value, left, right) -> Tree (f value, mapTree left f, mapTree right f)
+    | Tip(value) -> Tip(f value)
+
+let myTree = Tree(0, Tree(1, Tip(2), Tip(3)), Tip(4))
+
+let result = mapTree myTree (fun x -> x + 1)
