883f6df32af001c594fdc36822b267e6326781b9
tara-sova <tarpol19@gmail.com>
2015-04-20 15:17:08 +0300

    I'll see you soon

---
+++ 4term/testwork hash/testwork hash/Hash.fs
@@ -0,0 +1,37 @@
+﻿module Hash
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Text
+open System.Threading.Tasks
+
+/// Hash
+type Hash() = 
+
+/// Elements of a hashtable are lists. 
+    let mutable bucket = new List<List<int>>()
+
+/// Main hashfunction.
+    let mutable hashFunc : int -> int = fun x -> x + 1
+
+/// Create internal lists ih a hashtable
+    member h.Hash(someHashFunc : int -> int) = 
+        for i = 0 to 9 do
+            let intList = new List<int>()
+            bucket.Add(intList)
+        hashFunc <- someHashFunc
+
+/// Insert value in hashtable.
+    member h.InsertToHashTable(newElement : int) = 
+        bucket.[hashFunc(newElement)].Add(newElement)
+
+
+/// Check of element's exictence in a hashtable.
+    member h.Exist(newElement : int) = 
+        let res = bucket.[hashFunc(newElement)].Exists(fun x -> x = newElement)
+        res
+       
+/// Remove element from hashtable.
+    member h.Remove(newElement : int) = 
+        bucket.[hashFunc(newElement)].Remove(newElement)
+
---
+++ 4term/testwork hash/testwork hash/WorkProcess.fs
@@ -0,0 +1,31 @@
+﻿module WorkProcess
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Text
+open System.Threading.Tasks
+open Hash
+
+let f (newElement : int) =
+    let mutable sum = 0
+    for i = 0 to (newElement - 1) do
+        sum <- sum + i * i
+    let res = sum % 10
+    res
+
+let mutable hash = new Hash()
+hash.Hash(f)
+hash.InsertToHashTable(5)
+hash.InsertToHashTable(7)
+let hash1 = hash
+
+let is5 = hash.Exist(5)
+let is7 = hash.Exist(7)
+
+hash.Remove(7)
+let not7 = hash.Exist(7)
+
+let hash2 = hash
+hash2
+
+
