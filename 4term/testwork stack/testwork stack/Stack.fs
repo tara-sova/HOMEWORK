988449d95eca12aec48b498a4ec97d0a61d2bf3c
tara-sova <tarpol19@gmail.com>
2015-04-24 14:52:52 +0300

    stack is here

---
+++ 4term/testwork stack/testwork stack/Stack.fs
@@ -0,0 +1,35 @@
+﻿module Stack
+open System
+open System.Collections.Generic
+open System.Linq
+open System.Text
+open System.Threading.Tasks
+
+type Stack() = 
+     let stack = new List<int>()
+
+/// Push value to a Stack.
+     member s.Push(value : int) = stack.Add(value)
+
+/// Take Stack's top element.
+     member s.Pop() = 
+         if s.IsEmpty() then failwith "Stack is empty"
+         let element = stack.[stack.Count - 1]
+         stack.RemoveAt(stack.Count - 1)
+         element
+
+/// Check of existence elements in Stack.
+     member s.IsEmpty() = 
+         if (stack.Count = 0) then true
+         else false
+
+let s = new Stack()
+s.Push(3)
+s.Push(2)
+s.Push(1)
+let isE1 = s.IsEmpty()
+let res1 = s.Pop()
+let res2 = s.Pop()
+let res3 = s.Pop()
+let isE2 = s.IsEmpty()
+let fail = s.Pop()
