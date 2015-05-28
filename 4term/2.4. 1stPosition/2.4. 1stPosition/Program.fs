166e2e8ab58f7cf4eb6ac3214e2c0c067ccebaf7
tara-sova <tarpol19@gmail.com>
2015-05-28 03:52:10 +0300

    - correct result for element that doesn't exist

--- 4term/2.4. 1stPosition/2.4. 1stPosition/Program.fs
+++ 4term/2.4. 1stPosition/2.4. 1stPosition/Program.fs
@@ -1,11 +1,16 @@
 ﻿let rec search number list = 
-    if (List.head(list) = number) then list
-    elif ((List.length list = 1) && (List.head list <> number)) then
-        failwith "No such number in a list" 
-    else
-        search number list.Tail
+    if List.head(list) = number then Some(list)
+    elif (List.length list = 1 && List.head list <> number) then None
+    else 
+         match search number list.Tail with
+         | Some(list) -> Some(list)
+         | None -> None
 
-let rec getPosition number list = 
-    List.length(list) - List.length(search number list)
+let getPosition number list = 
+    let optionRes = search number list 
+    if optionRes.IsNone then "No such number"
+    else (List.length list - optionRes.Value.Length).ToString()
 
-getPosition 5 [1; 2; 3];;
+let list = [1;2;3;4;5]
+let result = getPosition 6 [1;2;3;4;5]
+result
