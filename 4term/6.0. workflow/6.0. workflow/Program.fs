69aa5ce175f6b332c598d7dbdcc5c3a77534d285
tara-sova <tarpol19@gmail.com>
2015-05-29 01:21:38 +0300

    strange hw

---
+++ 4term/6.0. workflow/6.0. workflow/Program.fs
@@ -0,0 +1,13 @@
+﻿type Rounding (precision : int) =
+    member this.Bind (x : float , rest : float -> float) =
+         rest (System.Math.Round (x, precision))  
+    member this.Return (x : float) = 
+        System.Math.Round(x, precision)
+
+let start = 
+   Rounding 3 {
+      let! a = 2.0 / 12.0
+      let! b = 3.5
+      return a / b
+   }
+start
