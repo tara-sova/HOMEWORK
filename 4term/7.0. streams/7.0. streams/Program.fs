cee6c31eaa5a657d7a453ef87a6923f27f1eb84e
tara-sova <tarpol19@gmail.com>
2015-05-29 02:33:06 +0300

    - some option

---
+++ 4term/7.0. streams/7.0. streams/Program.fs
@@ -0,0 +1,39 @@
+﻿open System.ComponentModel
+
+let mutable result = 0
+          
+type BackgroundWorkerForHundredStreams(i) =
+    let array = Array.create 1000000 1
+    let startPos = i * 10000
+    let finishPos = (i + 1) * 10000 - 1
+    
+    let worker = new BackgroundWorker()
+    let completed = new Event<_>()
+    let error = new Event<_>()
+    let cancelled = new Event<_>()          
+
+    do worker.DoWork.Add(fun args -> 
+                                     let rec count beginPos endPos  =
+                                         if worker.CancellationPending then
+                                            args.Cancel <- true
+                                         elif (endPos - beginPos > 0) then
+                                              result <- result + array.[beginPos]
+                                              count (beginPos + 1) endPos
+                                         else args.Result <- result   
+                                     count startPos finishPos)
+
+    do worker.RunWorkerCompleted.Add(fun args ->
+                                                 if args.Cancelled then cancelled.Trigger()
+                                                 elif args.Error <> null then error.Trigger args.Error
+                                                 else completed.Trigger (args.Result :?> int))
+
+    member b.WorkerCompleted = completed.Publish
+    member b.WorkerCancelled = cancelled.Publish
+    member b.WorkerError = error.Publish
+
+    member b.RunWorkerAsync() = worker.RunWorkerAsync()
+    member b.CancelAsync() = worker.CancelAsync()
+
+let backgroundWorkers = [|for i in 0..100 - 1 -> new BackgroundWorkerForHundredStreams(i)|]
+for i in 0..100 - 1 do backgroundWorkers.[i].RunWorkerAsync()
+printfn "%A" result
