6d7b514365739f1ec3995cc6ea0743b74e4b1be7
tara-sova <tarpol19@gmail.com>
2015-05-29 13:06:35 +0300

    - correct stream's finishes

--- 4term/7.0. streams/7.0. streams/Program.fs
+++ 4term/7.0. streams/7.0. streams/Program.fs
@@ -1,9 +1,11 @@
 ﻿open System.ComponentModel
 
 let mutable result = 0
+let completion = Array.create 100 false
           
 type BackgroundWorkerForHundredStreams(i) =
     let array = Array.create 1000000 1
+   // let completion = Array.create 100 false
     let startPos = i * 10000
     let finishPos = (i + 1) * 10000 - 1
     
@@ -19,7 +21,9 @@ type BackgroundWorkerForHundredStreams(i) =
                                          elif (endPos - beginPos > 0) then
                                               result <- result + array.[beginPos]
                                               count (beginPos + 1) endPos
-                                         else args.Result <- result   
+                                         else 
+                                             args.Result <- result   
+                                             completion.[i / 10000] <- true 
                                      count startPos finishPos)
 
     do worker.RunWorkerCompleted.Add(fun args ->
@@ -34,6 +38,18 @@ type BackgroundWorkerForHundredStreams(i) =
     member b.RunWorkerAsync() = worker.RunWorkerAsync()
     member b.CancelAsync() = worker.CancelAsync()
 
-let backgroundWorkers = [|for i in 0..100 - 1 -> new BackgroundWorkerForHundredStreams(i)|]
-for i in 0..100 - 1 do backgroundWorkers.[i].RunWorkerAsync()
-printfn "%A" result
+
+
+let backgroundWorkers = [for i in 0..99 -> new BackgroundWorkerForHundredStreams(1 + i * 10000)]
+for i in 0..99 do
+    (backgroundWorkers.Item i).RunWorkerAsync()
+    
+let allIsCompletion (flags : bool array) =
+    let mutable temp = true
+    for i in 0..(min((Array.length flags) - 1)  99) do
+        if flags.[i] = false then temp <- false
+    not temp    
+    
+while (allIsCompletion completion) do ()
+printfn "%d" result
+
