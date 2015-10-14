1dcd7df8f65b01528e8df05de3041ddd99bf46de
tara-sova <jmissexy@gmail.com>
2015-10-14 16:00:45 +0300

    oO

---
+++ num_analisys/homework1/homework1/Program.fs
@@ -0,0 +1,68 @@
+﻿open System.Collections.Generic
+
+
+let rec factorial n res = 
+    if n = 1.0 then res
+    else factorial (n - 1.0) (res * n)
+
+let p1 x = 
+    128.0 * (pown x 3) - 256.0 * (pown x 6) + 160.0 * (pown x 4) - 32.0 * (pown x 2) + 1.0
+
+let dP1 x = 
+    384.0 * (pown x 2) - 1536.0 * (pown x 5) + 640.0 * (pown x 3) - 64.0 * x
+
+let p2 x = 
+    (pown x 3) + 2.0 * (pown x 2) - 1.75 * x - 1.25
+
+let dP2 x = 
+    3.0 * (pown x 2) + 4.0 * x - 1.75
+
+let p3 x = //(pown x 3) + 4.0 * sin(x)
+    (pown x 3) + 4.0 * (x - ((pown x 3) / (factorial 3.0 1.0)) + ((pown x 5) / (factorial 5.0 1.0)))
+
+let dP3 x = 
+    3.0 * (pown x 2) + 4.0 * (1.0 - ((pown x 2) / (factorial 2.0 1.0)) + ((pown x 4) / factorial 4.0 1.0))
+ 
+let p x = x * x + x
+let dP x = 2.0 * x + 1.0
+
+let rec solve (f : float -> float) (dF : float -> float) (x_0 : float) counter = 
+    let eps : float = 0.00001 //0.001 //0.00001
+    let px = f x_0
+    let dPx = dF x_0
+    let x_k = x_0 - (px / dPx)
+    let difference = x_k - x_0
+    if abs difference > eps then 
+       System.Console.WriteLine("{0}    {1},    {2}", int32(counter), System.Math.Round(x_0, 4), System.Math.Round(px, 4))
+       solve f dF x_k (counter + 1.0)
+    else 
+        [counter; System.Math.Round(x_0, 4); System.Math.Round(px, 4)]
+    
+
+let act (interval : float * float) (f : float -> float) (dF : float -> float) =
+    let rank = (snd interval - fst interval) / 50.0
+    let list = new List<float>()
+    list.Insert(0, fst interval)
+    System.Console.WriteLine("k     x_k    P(x_k)")
+    for i = 0 to 49 do
+        if (f list.[i]) * (f (list.[i] + rank)) < 0.0 then
+           let resList = solve f dF list.[i] 0.0
+           System.Console.WriteLine("{0}    {1}   {2}\n\n", int32(resList.Head), resList.Tail.Head, resList.Tail.Tail.Head)
+        list.Insert(i + 1, list.[i] + rank)
+
+let mainFunc = 
+    System.Console.WriteLine("function = 128.0 * (x ^ 3) - 256.0 * (x ^ 6) + 160.0 * (x ^ 4) - 32.0 * (x ^ 2) + 1.0
+                              \ninterval = (0.058, 2.122)")
+    act (0.058, 2.122) p1 dP1
+    System.Console.WriteLine("\ninterval = (-0.47, -0.058)")
+    act (-0.47, -0.058) p1 dP1
+
+    System.Console.WriteLine("function = 3.0 * (x ^ 2) + 4.0 * x - 1.75
+                              \ninterval = (0.44, 2.3)")
+    act (0.44, 2.3) p2 dP2
+    System.Console.WriteLine("\ninterval = (-3.0, -0.38)")
+    act (-3.0, -0.38) p2 dP2
+
+    System.Console.WriteLine("function = (x ^ 3) + 4.0 * sin(x)
+                              \ninterval = (-1.0, 1.0)")
+    act (-1.0, 1.0) p3 dP3
---
+++ num_analisys/homework2_Lagrange/homework2_Lagrange/Program.fs
@@ -0,0 +1,126 @@
+﻿open System.Collections.Generic
+
+let Pi = 3.14159265//3.14
+
+let c x = cos(x)
+let dC x = cos(x)
+
+let c10 x = cos(10.0 * x)
+let dC10 x = 10000.0 * cos(10.0 *x)
+
+let rec factorial n = 
+    if n = 1 then 1
+    else n * factorial (n - 1)
+
+let LagrangePolinom (list : List<float * float>) (x : float) = 
+    let mutable mainRes = 0.0
+    let mutable result = 1.0
+    for i = 0 to list.Count - 1 do
+        for j = 0 to list.Count - 1 do
+            if (i <> j) then 
+                result <- result * ((x - (fst list.[j])) / ((fst list.[i]) - (fst list.[j])))
+            else result <- result + 0.0
+        mainRes <- mainRes + (result * (snd list.[i]))
+        result <- 1.0
+    mainRes
+
+let rec maximumMod f interval t (maxFunc : float) = 
+    if t >= 1.0 - 0.0001 then maxFunc
+    else
+        let x = ((1.0 - t) * (fst interval)) + (t * (snd interval))
+        let rrr = f x
+        if abs(f x) > maxFunc then 
+           maximumMod f interval (t + 0.001) (abs(f x))
+        else maximumMod f interval (t + 0.001) maxFunc
+
+let findA_i (f : float -> float) (dF : float -> float) (listOfPair : List<float * float>) interval x_i = 
+    let n = listOfPair.Count
+    let mutable w = 1.0
+    for i = 0 to (n - 1) do
+        let diff = x_i - (fst listOfPair.[i])
+        w <- w * (x_i - (fst listOfPair.[i]))
+    let maxOfMod = maximumMod dF interval 0.0 -100.0
+    let A_i = ( abs w / float(factorial n)) * maxOfMod
+    A_i
+
+let action (f : float -> float) dF (interval : float * float) (nodes : float list) = 
+    let n = 20.0
+    let list = new List<float * float>()
+    System.Console.WriteLine("x_i         L(x_i)         f(x_i)         |L - f|         A_i")
+    for j = 0 to nodes.Length - 1 do
+        list.Insert(j, (nodes.[j], f (nodes.[j])))
+    for i = 0 to 20 do
+        let x_i = fst interval + ((snd interval - fst interval) / n) * (float i)
+        let Lagrange_x_i = LagrangePolinom list x_i 
+        let difference = abs (Lagrange_x_i - f x_i)
+        let A_i = findA_i f dF list interval x_i
+        System.Console.WriteLine("{0}         {1}         {2}         {3}          {4}"
+                                 , System.Math.Round(x_i, 4), System.Math.Round(Lagrange_x_i, 4)
+                                 , System.Math.Round(f x_i, 4), System.Math.Round(difference, 4)
+                                 , System.Math.Round(A_i, 4) )
+
+let changeTable condition (list : float list) (interval : float * float)= 
+    if condition = 0 then 
+       System.Console.WriteLine ("\n\ntable for double number of nodes")
+       List.map (fun x -> x / 2.0) list
+    elif condition = 1 then 
+         System.Console.WriteLine ("\n\ntable for right part of interval")
+         List.map (fun x -> (snd interval - (x / 2.0))) list
+    else 
+        System.Console.WriteLine ("\n\ntable for left part of interval")
+        List.map (fun x -> fst interval + (x / 2.0)) list
+
+let mainFunc = 
+    let fList = [Pi/6.0; Pi/4.0; Pi/3.0; Pi/2.0]
+    let fInterval = (0.0, Pi/2.0)
+    System.Console.WriteLine("f = cos(x) \nsimple table")
+    action c dC fInterval fList
+    for i = 0 to 2 do
+        action c dC fInterval (changeTable i fList fInterval)
+
+    let gList = [-0.87; -0.69; -0.11; 0.31; 0.58; 0.97]
+    let gInterval = (-1.0, 1.0)
+    System.Console.WriteLine("\n\ng = cos(10 * x) \nsimple table")
+    action c10 dC10 gInterval gList
+    for i = 0 to 2 do
+        action c10 dC10 fInterval (changeTable i gList gInterval)
+ 
+
+
+
+
+
+
+
+/// test of Lagrange
+//let l1 (nodes : float list) f = 
+//    let list = new List<float * float>()
+//    for j = 0 to 4 do
+//        list.Insert(j, (nodes.[j], f (nodes.[j])))
+//    LagrangePolinom list 1.0 t
+//
+//let ll = l1 [-1.5; -0.75; 0.0; 0.75; 1.5] t
+//////ll
+////let r = action t dT (-2.0, 2.0) [-1.5; -0.75; 0.0; 0.75; 1.5]
+////r
+
+
+
+///// test of Lagrange Galya 
+//let l1 (nodes : float list) f = 
+//    let list = new List<float * float>()
+//    for j = 0 to 4 do
+//        list.Insert(j, (nodes.[j], f (nodes.[j])))
+//    LagrangePolinom list 1.0 
+//
+//let ll = l1 [-1.5 * Pi; -Pi; -Pi / 2.0; 0.0; Pi; 1.5 * Pi] c
+//ll
+////let r = action t dT (-2.0, 2.0) [-1.5; -0.75; 0.0; 0.75; 1.5]
+////r
+
+
+
+
+//let res1 = action t dT (-2.0, 2.0) [-1.5; -0.75; 0.0; 0.75; 1.5]
+//res1
+//let res = maximumMod s (0.0, Pi/4.0) 0.0 -100.0
